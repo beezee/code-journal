67c70de5fe08a6d4e36c5c44ca7e0bebf55a0509
Brian Zeligson <brian@bondlink.com>
2018-11-04 23:32:50 -0500

    main, put branch value before right descent

--- TailrecTT.hs
+++ TailrecTT.hs
@@ -5,12 +5,13 @@ import Data.List
 data Tree x = Branch x (Tree x) (Tree x) | Leaf x | TNil deriving Show
 
 cata :: Tree x -> [Tree x] -> [x] -> [x]
-cata (Branch x l r) defer acc = cata l (r: (Leaf x): defer) acc
+cata (Branch x l r) defer acc = cata l ((Leaf x): (r: defer)) acc
 cata (Leaf x) (d:defer) acc = cata d defer (acc ++ [x])
 cata (Leaf x) [] acc = (acc ++ [x])
 cata TNil (d:defer) acc = cata d defer acc
 cata TNil [] acc = acc
 
+run :: Tree Integer
 run = (Branch 42
   (Branch 7
     (Branch 8 (Leaf 17) (Leaf 9))
@@ -18,3 +19,5 @@ run = (Branch 42
   (Branch 3
     (Branch 5 (Leaf 7) (Leaf 8))
     (Branch 10 (Leaf 11) TNil)))
+
+main = putStrLn $ show (cata run [] [])
